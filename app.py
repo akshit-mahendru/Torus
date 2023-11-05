e045a420f54b25c32be99f050fe5ea05dea932a6
Akshit Mahendru <akshitmahendru15@gmail.com>
2023-11-05 10:57:03 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, redirect, url_for, send_file
+from views import views
+
+app = Flask(__name__)
+app.register_blueprint(views, url_prefix="/")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8040)
+
